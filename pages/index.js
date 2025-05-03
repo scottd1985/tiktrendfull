f5f71d38d6a3c8d7c23f2d222535de2b54cd16c1
scottd1985 <projecttiktok85@gmail.com>
2025-05-03 20:08:52 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>TikTrend is Live!</h1>
+      <p>Visit <code>/api/admin/scrape?token=mysecret123</code> to run the scraper.</p>
+    </div>
+  );
+}
