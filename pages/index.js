871ae112ba836a08efc7e7788325d5cae9556a71
scottd1985 <projecttiktok85@gmail.com>
2025-05-21 22:02:49 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>TikTrend is Live!</h1>
-      <p>Visit <code>/api/admin/scrape?token=mysecret123</code> to run the scraper.</p>
-    </div>
-  );
-}
